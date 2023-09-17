dc84b6c23d5983d99f52add328f59cf8bdcf5b78
Priyasivam53 <144479834+Priyasivam53@users.noreply.github.com>
2023-09-17 12:48:50 +0530

    Add files via upload

---
+++ unit 2.2.py
@@ -0,0 +1,23 @@
+class player:
+
+  def play(self):
+    print("the player is laying cricket")
+
+
+class batsman(player):
+
+  def play(self):
+    print("the basman is batting")
+
+
+class bowler(player):
+
+  def play(self):
+    print("the bowler is bowling")
+
+
+bat = batsman()
+bow = bowler()
+
+bat.play()
+bow.play()
